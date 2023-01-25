baaa638a1c17e15409ff6a560dff149fe51a42fa
Serge <iosnick@yandex.ru>
2023-01-25 23:17:01 +0700

    project added

---
+++ BasicsOfSurvival.git/BasicsOfSurvival.git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicsOfSurvival.git
+//
+//  Created by Sergei Vikhliaev on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
